fe3ec3dd977c824e8d6b82b56fe7ebdd4215b597
Trevor <tstike44@hotmail.com>
2019-10-08 16:12:34 -0700

    removed console log on line 33 and 48

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ $('#nav').affix({
 	var index=0;
 	$(document).scroll(function(){
 		var top = $('#skills').height()-$(window).scrollTop();
-		console.log(top)
+		// console.log(top)
 		if(top<-300){
 			if(index==0){	
 			
@@ -45,7 +45,7 @@ $('#nav').affix({
 			index++;
 		}
 	})
-	//console.log(nagativeValue)
+	// console.log(nagativeValue)
 	});
 
 
